90a1893e0e9fecaa11a2356d54525fe68ce1febc
camilo <cavb.1205@hotmail.com>
2019-05-24 14:25:32 -0400

    create views detailpost

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,7 @@ from posts import views
 urlpatterns = [
     #url for posts
     path('', views.PostsFeedView.as_view(), name='feed'),
+    path('<slug:title>',views.PostDetailView.as_view(), name='detail'),
     path('new/', views.new_post, name='new_post'),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from posts.models import Post
 from posts.forms import PostForm
@@ -9,11 +9,25 @@ from posts.forms import PostForm
 
 
 class PostsFeedView(ListView):
+    """show list posts order_by -created posts"""
 
     model = Post
     context_object_name = 'Posts'
     template_name = 'posts/feed.html'
     ordering = ('-created')
+    
+
+
+class PostDetailView(DetailView):
+    """show detail posts"""
+
+    model = Post
+    slug_field = 'title'
+    slug_url_kwarg = 'title'
+    template_name = 'posts/detail.html'
+    
+
+
 
 
 def new_post(request):
